f758ba98f5ac2b72a49440590182b2a96a18d746
los0220 <maciejj2000+github@gmail.com>
2025-12-09 03:33:55 +0100

    Improved - faster algoritm with sorting and custom Range struct 300 -> 234 μs

--- 05/part1.cpp
+++ 05/part1.cpp
@@ -1,116 +1,128 @@
+#include <algorithm>
 #include <chrono>
 #include <fstream>
 #include <iostream>
-#include <set>
 #include <sstream>
 #include <stdint.h>
 #include <string>
 #include <vector>
 
+struct Range
+{
+    int64_t start;
+    int64_t end;
+
+    bool operator <(Range const& rhs) const 
+    {
+        return (start < rhs.start) || (start == rhs.start && end < rhs.end);
+    }	
+};
 
 int solve(std::string const& fileName, bool verbose);
 int countAvailableFresh(
-	std::vector<int64_t> const& freshRangeStart, 
-	std::vector<int64_t> const& freshRangeEnd, 
-	std::vector<int64_t> const& availableIDs
+    std::vector<Range> const& freshRanges, 
+    std::vector<int64_t> const& availableIDs
 );
 
 int main(int argc, char* argv[])
 {
-	const std::string VERBOSE_FLAG { "-v" };
-	std::string fileName {};
-	bool verbose { false };
+    const std::string VERBOSE_FLAG { "-v" };
+    std::string fileName {};
+    bool verbose { false };
 
-	if (argc == 3 && argv[1] == VERBOSE_FLAG)
-	{
-		fileName = argv[2];
-		verbose = true;
-	}
-	else if (argc == 2)
-		fileName = argv[1];
-	else
-		return -1;
+    if (argc == 3 && argv[1] == VERBOSE_FLAG)
+    {
+        fileName = argv[2];
+        verbose = true;
+    }
+    else if (argc == 2)
+        fileName = argv[1];
+    else
+        return -1;
 
-	auto start = std::chrono::high_resolution_clock::now();
+    auto start = std::chrono::high_resolution_clock::now();
 
-	solve(fileName, verbose);
+    solve(fileName, verbose);
 
-	auto stop = std::chrono::high_resolution_clock::now();
-	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 
-	std::cout << "Time taken: " << duration.count() << " μs \n";
-		
-	return 0;
+    std::cout << "Time taken: " << duration.count() << " μs \n";
+        
+    return 0;
 }
 
 int solve(std::string const& fileName, bool verbose)
 {	
-	std::ifstream f(fileName);
-	std::vector<int64_t> freshRangeStart {};
-	std::vector<int64_t> freshRangeEnd	{};
-	std::vector<int64_t> availableIDs {};	
+    std::ifstream f(fileName);
+    std::string s {};
+    std::vector<Range> freshRanges {};
+    std::vector<int64_t> availableIDs {};	
+    int result { 0 };
+        
+    if (!f.is_open())
+    {
+        std::cerr << "Can't open " << fileName << "\n";
+        return -1;
+    }
 
-		
-	if (!f.is_open())
-	{
-		std::cerr << "Can't open " << fileName << "\n";
-		return -1;
-	}
+    while (std::getline(f, s))
+    {
+        int64_t first { 0 };
+        int64_t second { 0 }; 
+        char dash { '\0' };
+    
+        std::istringstream iss(s);
 
-	std::string s {};
-	while (std::getline(f, s))
-	{
-		int64_t first { 0 };
-		int64_t second { 0 }; 
-		char dash { '\0' };
-	
-		std::istringstream iss(s);
+        if ( !(iss >> first) )
+            continue;
 
-		if ( !(iss >> first) )
-			continue;
+        if (iss >> dash >> second && dash == '-')
+        {
+            if (verbose)
+                std::cout << first << " - " << second << "\n";
 
-		if (iss >> dash >> second && dash == '-')
-		{
-			if (verbose)
-				std::cout << first << " - " << second << "\n";
-			
-			freshRangeStart.push_back(first);
-			freshRangeEnd.push_back(second);
-		}
-		else
-		{
-			if (verbose)
-				std::cout << first << "\n";
-			
-			availableIDs.push_back(first);
-		}
-	}
+            freshRanges.push_back(Range{first, second});
+        }
+        else
+        {
+            if (verbose)
+                std::cout << first << "\n";
+            
+            availableIDs.push_back(first);
+        }
+    }
 
-	std::cout << countAvailableFresh(freshRangeStart, freshRangeEnd, availableIDs) << "\n";
-	
-	f.close();
+    std::sort(freshRanges.begin(), freshRanges.end());
+    std::sort(availableIDs.begin(), availableIDs.end());
+    
+    result = countAvailableFresh(freshRanges, availableIDs);
+    std::cout << result << "\n";
+    
+    f.close();
 
-	return 0;	
+    return 0;	
 }
 
 int countAvailableFresh(
-	std::vector<int64_t> const& freshRangeStart,
-	std::vector<int64_t> const& freshRangeEnd, 
-	std::vector<int64_t> const& availableIDs
+    std::vector<Range> const& freshRanges, 
+    std::vector<int64_t> const& availableIDs
 ) {
-	int freshCount {0};
+    int freshCount {0};
+    auto lastIt = freshRanges.begin();
+    
+    for (int64_t id : availableIDs)
+    {
+        for(auto it = lastIt; it != freshRanges.end(); ++it)
+        {
+            if (it->start <= id && id <= it->end)
+            {
+                lastIt = it;
+                ++freshCount;
+                break;
+            }
+        }
+    }
 
-	for (int64_t id : availableIDs)
-	{
-		for(size_t i = 0; i < freshRangeStart.size(); ++i)
-		{
-			if (freshRangeStart[i] <= id && id <= freshRangeEnd[i])
-			{
-				++freshCount;
-				break;
-			}
-		}
-	}
-
-	return freshCount;
+    return freshCount;
 }
