78fdfbef497e11c8ea4ebf37a843bc7cf4bb90b9
los0220 <maciejj2000+github@gmail.com>
2025-12-18 20:04:15 +0100

    Day 06: Implemented coding style fixes to better match the Google C++ style guide

--- 06/part1.cpp
+++ 06/part1.cpp
@@ -1,11 +1,20 @@
+#include <stdint.h>
 #include <chrono>
 #include <fstream>
 #include <iostream>
 #include <sstream>
-#include <stdint.h>
 #include <string>
+#include <string_view>
 #include <sstream>
 #include <vector>
+
+namespace {
+
+enum ReadState
+{
+    READ_RANGES,
+    READ_IDS
+};
 
 class Verbose
 {
@@ -13,143 +22,30 @@ public:
     Verbose(bool verbose): m_verbose(verbose) {}
 
     template <typename T>
-    void print(std::vector<T> vec) const
+    void print(const std::vector<T>& vec) const
     {
         if(!m_verbose)
             return;
 
         for (auto const& i : vec)
+        {
             std::cout << i << " ";
+        }
 
         std::cout << "\n";
     }
+
     operator bool() const
     {
         return m_verbose;
     }
+
 private:
     bool m_verbose;
 };
 
-int solve(std::string const& fileName, Verbose verbose);
-int64_t doTheMath(
-    const std::vector<std::vector<int64_t>>& numbers,
-    const std::vector<char>& operations
-);
-
-
-int main(int argc, char* argv[])
-{
-    const std::string VERBOSE_FLAG { "-v" };
-    std::string fileName {};
-    Verbose verbose(false);
-
-    if (argc == 3 && argv[1] == VERBOSE_FLAG)
-    {
-        fileName = argv[2];
-        verbose = Verbose(true);
-    }
-    else if (argc == 2)
-        fileName = argv[1];
-    else
-        return -1;
-
-    auto start = std::chrono::high_resolution_clock::now();
-
-    solve(fileName, verbose);
-
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-
-    std::cout << "Time taken: " << duration.count() << " μs \n";
-
-    return 0;
-}
-
-int solve(std::string const& fileName, Verbose verbose)
-{
-    std::ifstream f(fileName);
-    std::string line {};
-    std::istringstream iss {};
-    std::vector<std::vector<int64_t>> numbers {};
-    std::vector<char> operations {};
-    int64_t result { 0 };
-
-    if (!f.is_open())
-    {
-        std::cerr << "Can't open " << fileName << "\n";
-        return -1;
-    }
-
-    bool readNumbers { true };
-    while (std::getline(f, line))
-    {
-        if (readNumbers)
-        {
-            int matches { 0 };
-            int64_t inNumber { 0 };
-            std::vector<int64_t> tempNumbers {};
-
-            iss.str(line);
-
-            if (numbers.size() != 0)
-                tempNumbers.reserve(numbers[0].size());
-
-            while (iss >> inNumber)
-            {
-                tempNumbers.push_back(inNumber);
-                ++matches;
-            }
-
-            verbose.print(tempNumbers);
-
-            if (matches > 0)
-                numbers.push_back(std::move(tempNumbers));
-            else
-                readNumbers = false;
-
-            iss.clear();
-        }
-
-        if (!readNumbers)
-        {
-            char inChar { '\0' };
-
-            iss.str(line);
-
-            if (numbers.size() != 0)
-                operations.reserve(numbers[0].size());
-
-            while (iss >> inChar && (inChar == '*' || inChar == '+'))
-                operations.push_back(inChar);
-
-            verbose.print(operations);
-
-            iss.clear();
-        }
-    }
-
-    f.close();
-
-    for (auto it = numbers.begin(); it != numbers.end(); ++it)
-    {
-        if (operations.size() != it->size())
-        {
-            std::cerr << "Line lenghts do not match! \n";
-            return -1;
-        }
-    }
-
-    result = doTheMath(numbers, operations);
-    std::cout << result << "\n";
-
-    return 0;
-}
-
-int64_t doTheMath(
-    const std::vector<std::vector<int64_t>>& numbers,
-    const std::vector<char>& operations
-)
+int64_t doTheMath(const std::vector<std::vector<int64_t>>& numbers,
+                  const std::vector<char>& operations)
 {
     int64_t totalSum { 0 };
 
@@ -177,4 +73,126 @@ int64_t doTheMath(
     }
 
     return totalSum;
+}
+
+// TODO: split the file reading and parsing into a separate function
+int solve(const std::string& fileName, const Verbose verbose)
+{
+    std::ifstream f(fileName);
+    std::vector<std::vector<int64_t>> numbers {};
+    std::vector<char> operations {};
+
+    if (!f.is_open())
+    {
+        std::cerr << "Can't open " << fileName << "\n";
+        return 1;
+    }
+
+    ReadState readState { READ_RANGES };
+    std::string line {};
+    std::istringstream iss {};
+    while (std::getline(f, line))
+    {
+        if (readState == READ_RANGES)
+        {
+            int matches { 0 };
+            int64_t inNumber { 0 };
+            std::vector<int64_t> tempNumbers {};
+
+            iss.str(line);
+
+            if (numbers.size() != 0)
+            {
+                tempNumbers.reserve(numbers[0].size());
+            }
+
+            while (iss >> inNumber)
+            {
+                tempNumbers.push_back(inNumber);
+                ++matches;
+            }
+
+            verbose.print(tempNumbers);
+
+            if (matches > 0)
+            {
+                numbers.push_back(std::move(tempNumbers));
+            }
+            else
+            {
+                readState = READ_IDS;
+            }
+
+            iss.clear();
+        }
+
+        if (readState == READ_IDS)
+        {
+            char inChar { '\0' };
+
+            iss.str(line);
+
+            if (numbers.size() != 0)
+                operations.reserve(numbers[0].size());
+
+            while (iss >> inChar && (inChar == '*' || inChar == '+'))
+                operations.push_back(inChar);
+
+            verbose.print(operations);
+
+            iss.clear();
+        }
+    }
+
+    f.close();
+
+    for (const std::vector<int64_t>& v : numbers)
+    {
+        if (operations.size() != v.size())
+        {
+            std::cerr << "Line lenghts do not match! \n";
+            return 1;
+        }
+    }
+
+    int64_t result = doTheMath(numbers, operations);
+    std::cout << result << "\n";
+
+    return 0;
+}
+
+}  // namespace
+
+int main(int argc, char* argv[])
+{
+    static constexpr std::string_view VERBOSE_FLAG { "-v" };
+    std::string fileName {};
+    Verbose verbose(false);
+
+    if (argc == 3 && argv[1] == VERBOSE_FLAG)
+    {
+        fileName = argv[2];
+        verbose = Verbose(true);
+    }
+    else if (argc == 2)
+    {
+        fileName = argv[1];
+    }
+    else
+    {
+        std::cerr << "No input passed! \n";
+        return 1;
+    }
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    solve(fileName, verbose);
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
+        stop - start);
+
+    std::cout << "Time taken: " << duration.count() << " μs \n";
+
+    return 0;
 }
--- 06/part2.cpp
+++ 06/part2.cpp
@@ -1,168 +1,52 @@
+#include <stdint.h>
 #include <chrono>
 #include <fstream>
 #include <iostream>
 #include <sstream>
-#include <stdint.h>
 #include <string>
+#include <string_view>
 #include <vector>
+
+namespace {
 
 struct Equation
 {
     std::vector<int> numbers;
     char operation;
 
-    int64_t calculate(void) const;
-    void print(void) const;
+    int64_t calculate(void) const
+    {
+        int64_t result { -1 };
+        switch(operation)
+        {
+        case '*':
+            result = 1;
+            for (int n : numbers)
+                result *= int64_t{n};
+            break;
+        case '+':
+            result = 0;
+            for (int n : numbers)
+                result += int64_t{n};
+            break;
+        }
+
+        return result;
+    }
 };
 
-int solve(std::string const& fileName, bool verbose);
-int parseInput(
-    const std::vector<std::string>& rows,
-    std::vector<Equation>& equations
-);
-int64_t doTheMath(std::vector<Equation>& equations);
-
-
-int main(int argc, char* argv[])
+std::ostream& operator <<(std::ostream& os, const Equation& eq)
 {
-    const std::string VERBOSE_FLAG { "-v" };
-    std::string fileName {};
-    bool verbose { false };
+    os << eq.operation << "  ";
+    for (int n : eq.numbers)
+        os << n << " ";
 
-    if (argc == 3 && argv[1] == VERBOSE_FLAG)
-    {
-        fileName = argv[2];
-        verbose = true;
-    }
-    else if (argc == 2)
-        fileName = argv[1];
-    else
-        return -1;
+    os << "= " << eq.calculate();
 
-    auto start = std::chrono::high_resolution_clock::now();
-
-    solve(fileName, verbose);
-
-    auto stop = std::chrono::high_resolution_clock::now();
-    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
-
-    std::cout << "Time taken: " << duration.count() << " μs \n";
-
-    return 0;
+    return os;
 }
 
-int64_t Equation::calculate(void) const
-{
-    int64_t result { -1 };
-    switch(operation)
-    {
-    case '*':
-        result = 1;
-        for (int n : numbers)
-            result *= static_cast<int64_t>(n);
-        break;
-    case '+':
-        result = 0;
-        for (int n : numbers)
-            result += static_cast<int64_t>(n);
-        break;
-    }
-
-    return result;
-}
-
-void Equation::print(void) const
-{
-    std::cout << operation << "  ";
-    for (int n : numbers)
-        std::cout << n << " ";
-
-    std::cout << "= " << calculate() << "\n";
-}
-
-int solve(std::string const& fileName, bool verbose)
-{
-    std::ifstream f(fileName);
-    std::string line {};
-    std::vector<std::string> rows {};
-    std::vector<Equation> equations {};
-    int64_t result { 0 };
-
-    if (!f.is_open())
-    {
-        std::cerr << "Can't open " << fileName << "\n";
-        return -1;
-    }
-
-    while (std::getline(f, line))
-        rows.push_back(line);
-
-    f.close();
-
-    for (auto it = rows.begin()+1; it != rows.end(); ++it)
-    {
-        if (rows.begin()->size() != it->size())
-        {
-            std::cerr << "Line lenghts do not match! \n";
-            return -1;
-        }
-    }
-
-    parseInput(rows, equations);
-
-    if (verbose)
-    {
-        for (const Equation& eq : equations)
-            eq.print();
-    }
-
-    result = doTheMath(equations);
-    std::cout << result << "\n";
-
-    return 0;
-}
-
-int parseInput(
-    const std::vector<std::string>& rows,
-    std::vector<Equation>& equations
-)
-{
-    size_t rowLen { rows[0].size() };
-    size_t colLen { rows.size() };
-    Equation eq {{}, '\0'};
-
-    for (size_t x = 0; x < rowLen; ++x)
-    {
-        char input { '\0' };
-        int num { 0 };
-
-        for (size_t y = 0; y < colLen; ++y)
-        {
-            input = rows[y][rowLen - x - 1];
-
-            if ('0' <= input && input <= '9')
-            {
-                num *= 10;
-                num += static_cast<int>(input - '0');
-            }
-            else if (input == '+' || input == '*')
-                break;
-        }
-
-        if (num > 0)
-            eq.numbers.push_back(num);
-
-        if (input == '+' || input == '*')
-        {
-            eq.operation = input;
-            equations.push_back(std::move(eq));
-        }
-    }
-
-    return 0;
-}
-
-int64_t doTheMath(std::vector<Equation>& equations)
+int64_t doTheMath(const std::vector<Equation>& equations)
 {
     int64_t totalSum { 0 };
 
@@ -172,4 +56,127 @@ int64_t doTheMath(std::vector<Equation>& equations)
     }
 
     return totalSum;
+}
+
+// TODO: make a better reverse itteration
+// TODO: implement error handling
+void parseInput(const std::vector<std::string>& rows,
+               std::vector<Equation>& equations)
+{
+    const size_t ROW_LEN { rows[0].size() };
+    const size_t COL_LEN { rows.size() };
+    Equation eq { {}, '\0' };
+
+    for (size_t x = 0; x < ROW_LEN; ++x)
+    {
+        char input { '\0' };
+        int num { 0 };
+
+        for (size_t y = 0; y < COL_LEN; ++y)
+        {
+            input = rows[y][ROW_LEN - x - 1];
+
+            if ('0' <= input && input <= '9')
+            {
+                num *= 10;
+                num += int{input - '0'};
+            }
+            else if (input == '+' || input == '*')
+            {
+                break;
+            }
+        }
+
+        if (num > 0)
+        {
+            eq.numbers.push_back(num);
+        }
+
+        if (input == '+' || input == '*')
+        {
+            eq.operation = input;
+            equations.push_back(std::move(eq));
+        }
+    }
+}
+
+int solve(const std::string& fileName, bool verbose)
+{
+    std::ifstream f(fileName);
+    std::vector<std::string> rows {};
+    std::vector<Equation> equations {};
+
+    if (!f.is_open())
+    {
+        std::cerr << "Can't open " << fileName << "\n";
+        return 1;
+    }
+
+    std::string line {};
+    while (std::getline(f, line))
+    {
+        rows.push_back(line);
+    }
+
+    f.close();
+
+    for (auto it = rows.begin()+1; it != rows.end(); ++it)
+    {
+        if (rows.begin()->size() != it->size())
+        {
+            std::cerr << "Line lenghts do not match! \n";
+            return 1;
+        }
+    }
+
+    parseInput(rows, equations);
+
+    if (verbose)
+    {
+        for (const Equation& eq : equations)
+        {
+            std::cout << eq << "\n";
+        }
+    }
+
+    int64_t result = doTheMath(equations);
+    std::cout << result << "\n";
+
+    return 0;
+}
+
+}  // namespace
+
+int main(int argc, char* argv[])
+{
+    static constexpr std::string_view VERBOSE_FLAG { "-v" };
+    std::string fileName {};
+    bool verbose { false };
+
+    if (argc == 3 && argv[1] == VERBOSE_FLAG)
+    {
+        fileName = argv[2];
+        verbose = true;
+    }
+    else if (argc == 2)
+    {
+        fileName = argv[1];
+    }
+    else
+    {
+        std::cerr << "No input passed! \n";
+        return 1;
+    }
+
+    auto start = std::chrono::high_resolution_clock::now();
+
+    solve(fileName, verbose);
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
+        stop - start);
+
+    std::cout << "Time taken: " << duration.count() << " μs \n";
+
+    return 0;
 }
